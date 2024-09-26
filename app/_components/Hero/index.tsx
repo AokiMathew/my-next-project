179df52ae70ee04d3c061bceb1a2ea6bd58dad84
AokiMathew <aoki.takatoshi@gmail.com>
2024-09-26 21:48:15 +0900

    第4章まで完了

--- app/_components/Hero/index.tsx
+++ app/_components/Hero/index.tsx
@@ -8,18 +8,18 @@ type Props={
 
 export default function Hero({title,sub}:Props){
     return (
-        <section className={styles.top}>
-        <div>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.sub}>{sub}</p>
-        </div>
-        <Image
-          className={styles.bgimg}
-          src="/img-mv.jpg"
-          alt=""
-          width={4000}
-          height={1200}
-        />
+        <section className={styles.container}>
+          <div>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.sub}>{sub}</p>
+          </div>
+          <Image
+            className={styles.bgimg}
+            src="/img-mv.jpg"
+            alt=""
+            width={4000}
+            height={1200}
+          />
       </section>
     )
 }
